873a9ab3718d18d7f0b626a35c714f76dc5b7db1
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-03 14:45:51 +0100

    Migrated to db.

--- Data/Entities/UserEntity.cs
+++ Data/Entities/UserEntity.cs
@@ -2,11 +2,11 @@
 
 public class UserEntity
 {
-    public int UserId { get; set; }
+    public int Id { get; set; }
     public EmployeeEntity Employee { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string SaltKey { get; set; } = null!;
 
-    public int RoleId { get; set; } = null!;
+    public int RoleId { get; set; }
     public RoleEntity Role { get; set; } = null!;
 }
