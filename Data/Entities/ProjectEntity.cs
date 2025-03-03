8fae39b833bbcfd61945a9d25ed44d0daf7246e5
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-03-03 11:07:19 +0100

    Added entities

---
+++ Data/Entities/ProjectEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class ProjectEntity
+{
+    public int Id { get; set; }
+    public string ProjectName { get; set; } = null!;
+    public string? ProjectDescription { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime StartDate { get; set; }
+   
+    [Column(TypeName = "date")]
+    public DateTime? EndDate { get; set; }
+
+    public int StatusId { get; set; }
+    public int ClientId { get; set; }
+    public int ProjectManagerId { get; set; }
+    public int? ProductId { get; set; }
+    public int ProjectTypeId { get; set; } = null!;
+
+    public StatusEntity Status { get; set; } = null!;
+    public ClientEntity Client { get; set; } = null!;
+    public EmployeeEntity ProjectManager { get; set; } = null!;
+    public ProductEntity? Product { get; set; }
+    public ProjectTypeEntity ProjectType { get; set; } = null!;
+
+}
+
+
